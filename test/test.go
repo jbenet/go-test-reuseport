6290220b6d663c44cf6555fc4212f8263f083a05
Juan Batiz-Benet <juan@benet.ai>
2015-01-18 12:10:50 -0800

    print whats going on

--- test/test.go
+++ test/test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io"
+	"net"
 	"os"
 
 	// ma "github.com/jbenet/go-multiaddr"
@@ -16,9 +17,11 @@ func main() {
 
 	l1, err := Listen("tcp4", "0.0.0.0:11111")
 	maybeDie(err)
+	fmt.Printf("listening on %s\n", l1.Addr())
 
 	l2, err := Listen("tcp4", "0.0.0.0:22222")
 	maybeDie(err)
+	fmt.Printf("listening on %s\n", l2.Addr())
 
 	// // make sure port can be reused. TOOD this doesn't work...
 	// err = setSocketReuseListener(l1)
@@ -36,20 +39,24 @@ func main() {
 	go func() {
 		l2to1foo, err := l2.Accept()
 		maybeDie(err)
+		fmt.Printf("%s accepted conn from %s\n", addrStr(l2.Addr()), addrStr(l2to1foo.RemoteAddr()))
 
 		fmt.Println("safe")
 
 		l1to2bar, err := l1.Accept()
 		maybeDie(err)
+		fmt.Printf("%s accepted conn from %s\n", addrStr(l1.Addr()), addrStr(l1to2bar.RemoteAddr()))
 
 		io.Copy(l1to2bar, l2to1foo)
 	}()
 
 	d1to2foo, err := d1.Dial("tcp4", "127.0.0.1:22222")
 	maybeDie(err)
+	fmt.Printf("dialing from %s to %s\n", d1.LocalAddr, "127.0.0.1:22222")
 
 	d2to1bar, err := d2.Dial("tcp4", "127.0.0.1:11111")
 	maybeDie(err)
+	fmt.Printf("dialing from %s to %s\n", d2.LocalAddr, "127.0.0.1:11111")
 
 	go io.Copy(d1to2foo, os.Stdin)
 	io.Copy(os.Stdout, d2to1bar)
@@ -64,4 +71,8 @@ func maybeDie(err error) {
 	if err != nil {
 		die(err)
 	}
+}
+
+func addrStr(a net.Addr) string {
+	return fmt.Sprintf("%s/%s", a.Network(), a)
 }
